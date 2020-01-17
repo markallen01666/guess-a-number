3cbbf73e5646a2c46aca8a3d00438e71cc1b8d3d
Mark Allen <markallen.webdeveloper@gmail.com>
2020-01-17 12:16:14 +0000

    Completed lesson 65: switching screens

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [visibleScreen, setVisibleScreen] = useState("StartGameScreen");
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  // Screen display choice
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  } 
+
   return (
     <View style={styles.screen}>
       <Header title="Guess A Number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
@@ -16,5 +31,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1
-  }  
+  }
 });
